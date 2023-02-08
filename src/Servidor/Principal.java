96a5bba2b47976388be73113c8327683d7ed1984
Manuel <manuel.perez-perez@iessampedro.org>
2023-02-08 21:20:19 +0100

    primera subida

---
+++ src/Servidor/Principal.java
@@ -0,0 +1,77 @@
+package Servidor;
+import java.net.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Principal {
+	
+	
+
+	public static void main(String[] args) {
+		//variables
+		ServerSocket socket;
+		Random r= new Random();
+		int filas=r.nextInt(5)+2;
+		int columnas=r.nextInt(5)+2;
+		int[][] enteros= new int[filas][columnas];
+		filas=r.nextInt(5)+2;
+		columnas=r.nextInt(5)+2;
+		double [][] decimal= new double[filas][columnas];
+		cargaMatrizes(enteros, decimal); 
+
+		
+		try {
+			 
+			 socket= new ServerSocket(7000);
+			//lanzo primero mi propio servicio para poder usar la consola
+			SirveAConsola meSirvo= new SirveAConsola(enteros, decimal);
+			meSirvo.start();
+			System.out.println("Esperando que alguien se conecte");
+			
+			//pongo el servidor a la espera de que algun cliente se conecte
+			Socket socket_cli = socket.accept();	
+			System.out.println("Cliente conectado");
+			
+			//una vez conectado, lanzo su hilo
+			SirveACliente cliente= new SirveACliente(socket_cli, enteros, decimal);
+				cliente.start();
+
+		 }catch (Exception e) {
+			// TODO: handle exception
+		}
+		 
+		 
+		 
+		 
+		
+		 
+		
+	}
+
+	public static void cargaMatrizes(int[][] enteros, double[][] decimal) {
+		Random r= new Random();
+		
+		//rellenamos arrays
+		 for (int i = 0; i < enteros.length; i++) {
+			 for (int j = 0; j < enteros[i].length; j++) {
+				enteros[i][j]= r.nextInt(9)+1;
+				
+				
+			}
+			
+			
+		}
+		 
+	
+		 for (int i = 0; i < decimal.length; i++) {
+			 for (int j = 0; j < decimal[i].length; j++) {
+				decimal[i][j]=Double.valueOf(r.nextInt(101)+1)/100;
+				;
+			}
+		
+		}
+	}
+
+}
